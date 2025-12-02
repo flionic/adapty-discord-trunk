7de3987336abb6c4a085d99f84f910b1f1c9027f
flionic <bionic.mods@gmail.com>
2025-12-02 21:40:51 +0200

    Fix

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ discord_webhook_url = "https://discord.com/api/webhooks/1445192620312625328/IPJJ
 adapty_avatar_url = "https://cdn.discordapp.com/attachments/1008830273837875211/1445195577993461840/favicon_black_1.png?ex=692f76c4&is=692e2544&hm=95f31ded3915ebb3897a85034f8706e3e98c348fc9d7f9c59a301c89412e1ea2"
 adapty_url = "https://app.adapty.io/profiles/users"
 
-@app.route("/webhook", methods=["POST"])
+@app.route("/webhook", methods=["POST", "GET"])
 def webhook():
+    if request.method == "GET":
+        return jsonify({"success": True})
+
     print("=== Webhook received ===")
 
     # Получаем JSON из запроса
